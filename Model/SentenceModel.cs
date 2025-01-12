607e39758e1dcf8546727ab95c258e0e6a3aa4f0
GabrielmAlves <gabrielmalves10@gmail.com>
2025-01-12 14:14:56 -0300

    creating UI

--- Model/SentenceModel.cs
+++ Model/SentenceModel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace SentimentAnalysis.Sentiment_Analysis.Model
 {
-    class SentenceModel
+    public class SentenceModel
     {
+        string sentence { get; set; }
+        List<string> sentenceTokens { get; set; }
+        int positiveFrequency {  get; set; }
+        int negativeFrequency { get; set; }
+        List<string> negativeWords { get; set;}
+        List<string> positiveWords { get; set;}
     }
 }
